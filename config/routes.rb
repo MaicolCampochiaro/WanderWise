feb499e28607a081ff74c54fdc7eeef1b1a4fc65
kane abdoul <kaneaziz@gmail.com>
2023-11-28 09:41:36 +0100

    controller models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+    resources :activities do
+    resources :activity_statuses, only: [:index, :show, :create, :update, :destroy]
+    end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
