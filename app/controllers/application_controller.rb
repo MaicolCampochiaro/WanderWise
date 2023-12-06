7e9bc68a9ff853972f868b7ce31236fd98e29d76
MaicolCampochiaro <maicolcampochiaro@gmail.com>
2023-12-06 10:58:05 +0100

    Stufff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || trips_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "trips#homepage", as: :homepage
   get '/locations/:id' => 'locations#index', as: :locations
   get '/location/:id' => 'locations#create_location', as: 'create_location'
   resources :trips, except: [:update] do
-    resources :flights, only: [:index, :new, :create], as: :flights
+    resources :flights, only: [:index, :new], as: :flights
     resources :hotels, only: [:index, :create, :show], as: :hotels
     resources :activities, only: [:index, :create, :show], as: :activities
   end
+  get '/trips' => 'trips#index', as: 'my_trips'
   get '/trips/:trip_id/:query' => 'trips#show', as: 'overview'
   post '/trips' => 'trips#create', as: 'create'
   get '/trips/:trip_id/hotels/:hotel_id' => 'hotels#show', as: 'hotel_show'
